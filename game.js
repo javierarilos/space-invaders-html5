ef7c8f99ca87ab249d9f1c0fca6c1f33be55d069
Javier Arias <javier.arias@skyscanner.net>
2017-08-12 15:48:48 +0200

    player object created

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
 ;(function() {
   var Game = function(canvasId) {
     var canvas = document.getElementById(canvasId);
-    var screen = canvas.getContext('2d');
+    var scr = canvas.getContext('2d');
     var gameSize = {
       x: canvas.width,
       y: canvas.height
     };
 
     var self = this;
+    self.bodies = [new Player(self, gameSize)];
     var tick = function(){
       self.update();
-      self.draw(screen, gameSize);
+      self.draw(scr, gameSize);
       requestAnimationFrame(tick);
     };
 
@@ -23,10 +24,36 @@
       console.log('hi!!')
 
     },
-    draw: function(screen, gameSize) {
-
+    draw: function(scr, gameSize) {
+      for (var i = 0; i < this.bodies.length; i++) {
+        drawRect(scr,  this.bodies[i]);
+      }
     }
 
+  };
+
+  var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = {x: 15, y: 15};
+    this.center = {
+      x: gameSize.x / 2,
+      y: gameSize.y - this.size.x
+    };
+  };
+
+  Player.prototype = {
+    update: function() {
+
+    }
+  };
+
+  var drawRect = function(scr, body) {
+    scr.fillRect(
+      body.center.x - body.size.x/2,
+      body.center.y - body.size.y/2,
+      body.size.x,
+      body.size.y
+    );
   };
 
   window.onload = function() {
