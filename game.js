48fc51a74f80b1605a8ee4c8423518cc662d1803
Javier Arias <javier.arias@skyscanner.net>
2017-08-12 15:15:02 +0200

    load Game object

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-console.log("hi from the game.js!!")
+;(function() {
+  var Game = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = {
+      x: canvas.width,
+      y: canvas.height
+    };
+
+    console.log("game loaded OK");
+
+  };
+
+  Game.prototype = {
+
+  };
+
+  window.onload = function() {
+    new Game('screen');
+
+  };
+
+})();
